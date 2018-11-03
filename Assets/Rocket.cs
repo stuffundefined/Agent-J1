0222c89838e1bf9e36624ac5e75fa98c1ac79db2
stuffundefined <41525027+stuffundefined@users.noreply.github.com>
2018-11-03 15:59:44 -0400

    Level switching

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,30 +1,41 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class Rocket : MonoBehaviour {
 	[SerializeField] float RCSThrust = 100f;
 	[SerializeField] float mainThrust = 100f;
 	Rigidbody rigidBody;
 	AudioSource audioSource;
+	enum State { Alive, Failed, Teleporting };
+	State state = State.Alive;
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
 		audioSource = GetComponent<AudioSource>();
 	}
 	void Update () {
-		Rotate();
-		Thrust();
+		if (state != State.Failed) {
+			Rotate();
+			Thrust();
+		}
 	}
 	private void OnCollisionEnter(Collision collision) {
-		switch(collision.gameObject.tag) {
+		switch (collision.gameObject.tag) {
 			case "Friendly":
 				break;
 			case "Finish":
-				print("yay");
+				state = State.Teleporting;
+				Invoke("LoadNext", 1f);
 				break;
 			default:
-				print("die");
+				state = State.Failed;
+				Invoke("Fail", 1f);
 				break;
 		}
+	}
+	private void Fail() {
+		SceneManager.LoadScene(0);
+	}
+	private void LoadNext() {
+		SceneManager.LoadScene(1);
 	}
 	private void Rotate() {
 		rigidBody.freezeRotation = true;
@@ -37,13 +48,13 @@ public class Rocket : MonoBehaviour {
 		rigidBody.freezeRotation = false;
 	}
 	void Thrust() {
-		if (Input.GetKey(KeyCode.Space)) {
-			rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-			if (!audioSource.isPlaying) {
-				audioSource.Play();
+			if (Input.GetKey(KeyCode.Space)) {
+				rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+				if (!audioSource.isPlaying) {
+					audioSource.Play();
+				}
+			} else {
+				audioSource.Stop();
 			}
-		} else {
-			audioSource.Stop();
-		}
 	}
 }
