3c94d62c60a08da8d9cb1f1db16b7d2651d0c358
stuffundefined <41525027+stuffundefined@users.noreply.github.com>
2018-12-18 19:24:08 -0500

    final

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 [DisallowMultipleComponent] public class Oscillator : MonoBehaviour {
-	[SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+	[SerializeField] Vector3 movementVector = new Vector3(10f, 0f, 0f);
 	[SerializeField] float period = 2f;
 	[Range(0, 1)] [SerializeField] float movementFactor;
 	Vector3 startingPos;
